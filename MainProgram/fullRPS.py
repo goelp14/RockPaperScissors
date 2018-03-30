19a8f1c2d8f349aa125a8921df8cdfb916153660
PGODULTIMATE <pranavgoel11@gmail.com>
2018-03-30 10:49:04 -0500

    Update fullRPS.py

--- MainProgram/fullRPS.py
+++ MainProgram/fullRPS.py
@@ -418,9 +418,9 @@ def bigbang():
   global probabilitiesRPS
   print(
   """
-    As a wise man said:
+As a wise man said:
     
-    Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.
+Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.
   """)
   choices = ["Rock","Paper","Scissors","Lizard","Spock"]
   choi = ['r','p','sc','l','sp']
